c877d64d573eb8e824dbe50ce52296c02b72e392
CRISTIAN DAVID CUADROS ACOSTA <cristiancuadros284859@correo.itm.edu.co>
2023-09-14 15:20:38 -0500

    Todo

---
+++ Veterinary.API/Controllers/OwnersController.cs
@@ -0,0 +1,72 @@
+﻿using Veterinary.API.Data;
+using Owner.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Veterinary.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/owners")]
+    public class OwnersController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+
+        public OwnersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //GET CON LISTA
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            //200 TODO OK
+            //204
+            //400
+            //401
+            //500 ERROR DE CODIGO
+            //AWAIT=ESPERAR
+            //DEBE RESPONDER AL CONTEXT DE CIENTIFICOS
+            //CUANDO EL METODO ES GET CON PARAMETROS SOLO MUESTRA ALGUNOS DATOS
+            //CUANDO EL METODO ES GET SIN PARAMETROS MUESTRA TODOS LOS DATOS (SELECT * FROM CIENTIFICOS)
+            return Ok(await _context.Owners.ToListAsync());
+
+
+        }
+
+        // METODO GET CON PARAMETROS
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            //200 TODO OK
+            //204
+            //400
+            //401
+            //500 ERROR DE CODIGO
+            //AWAIT=ESPERAR
+            //DEBE RESPONDER AL CONTEXT DE CIENTIFICOS
+            //CUANDO EL METODO ES GET CON PARAMETROS SOLO MUESTRA ALGUNOS DATOS
+            //CUANDO EL METODO ES GET SIN PARAMETROS MUESTRA TODOS LOS DATOS (SELECT * FROM CIENTIFICOS)
+            await _context.Owners.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok();
+
+
+        }
+
+        //EL POST PUEDE MODIFICAR LA TABLA
+        //INSERTAR UN REGISTRO
+        [HttpPost]
+        public async Task<ActionResult> Post(Owner owner)
+        {
+
+
+            _context.Add(owner);
+            await _context.SaveChangesAsync();
+            return Ok(owner);
+
+        }
+    }
---
+++ Veterinary.Shared/Veterinary.Shared/Entities/Owner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinary.Shared.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "El documento es requerido")]
+
+        public string Document { get; set; } = null;
+        public string FirstName { get; set; } = null;
+
+        public string LastName { get; set; } = null;
+
+        public string FixedPhone { get; set; } = null;
+
+        public string Cellphone { get; set; } = null;
+
+        public string Address { get; set; } = null;
+
+        public string Fullname => $"{FirstName}" +
+            $"{LastName}";
+    }
+}
